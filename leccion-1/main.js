9046b5b0ddf37faf8d02ec81b0632fd9c02c5875
JDIAZ <jdiaz@southend.com.ar>
2022-11-08 17:33:08 -0300

    ejecicios del 1 al 4

---
+++ leccion-1/main.js
@@ -0,0 +1,26 @@
+console.log("Hola mundo! archivo .js")
+
+let nombre = "juan manuel diaz colodrero";
+let edad = 27;
+let mayorDeEdad = true;
+let direccion = {
+    calle: "14 de julio",
+    numero: 3551,
+    barrio: "lanus oeste",
+    cp: 1824
+};
+let coloresFavoritos = ["rojo","negro","blanco"];
+let lenguajesFavoritos = ["java","javascript","c#"];
+let mejorLenguaje = lenguajesFavoritos[0];
+let peorLenguaje = lenguajesFavoritos[2];
+
+let numero1 = 20;
+let numero2 = 10;
+
+console.log(numero1+numero2);
+
+let saludo = "hola"
+
+if (saludo == "hola"){
+    console.log("Ha saludado")
+}
